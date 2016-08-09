ad7b36da10c043af0aeb5eb2699639edcb20af86
charujaiswal <clmjaiswal@gmail.com>
2016-08-09 18:34:09 -0400

    Using variable_scope to create and initialize variables for weights and biases

--- lstm.py
+++ lstm.py
@@ -1,6 +1,5 @@
 '''
 Character-level LSTM to generate text.
-
 Uses built-in rnn_cell from Tensorflow
 
 Characteristics :
@@ -10,11 +9,8 @@ Characteristics :
     -Train with batches of 100 examples
     -Truncated BPTT length of 100 char
 
-
 Based on:
-
 https://github.com/hunkim/word-rnn-tensorflow/blob/master/model.py   [I'm not using seq2seq]
-
 https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
 
 
@@ -29,9 +25,6 @@ from tensorflow.python.ops import rnn_cell
 
 training_iters=100000
 
-# TF Graph input
-input_data= tf.placeholder(tf.float32, [batch_size, seq_length])
-targets=tf.placeholder(tf.float32,[batch_size, seq_length])
 
 class Model():
     def __init__(self, args):
@@ -41,6 +34,7 @@ class Model():
         args.seq_length= 20 # number of steps to unroll the LSTM
         args.num_layers= 2
         args.learning_rate= 0.001
+        args.vocab_size= #todo: how should I represent vocab? 1-Hot?
 
         # Create multi-layer LSTM cell
         lstm= rnn_cell.BasicLSTMCell(args.lstm_size) # size of a single LSTM cell
@@ -50,6 +44,25 @@ class Model():
         # Create placeholders for inputs and targets
         self.input_data= tf.placeholder(tf.float32,[args.batch_size, args.seq_length]) #TODO: should data be float or int?
         self.targets = tf.placeholder(tf.float32,[args.batch_size,args.seq_length])
+
+        with tf.variable_scope('lstm-'):
+            ''' The purpose of using variable_scope is to easily share named variables when creating a graph.
+            Instead of manually naming all the variables. This allows for scaling up (i.e. increase num_layers or size)
+
+            see: https://www.tensorflow.org/versions/r0.10/how_tos/variable_scope/index.html
+            '''
+
+            # Variables created here will be named "lstm-/weights", "lstm-/biases"
+
+            # Create variable named 'weights'. Uses an initializer instead of passing the value directly as in tf.Variable
+            weights= tf.get_variable('weights',[args.lstm_size,args.vocab_size], initializer=tf.random_normal_initializer())
+
+            # Create a variable named "biases". Initialize to value of 0
+            biases = tf.get_variable('biases',args.vocab_size, initializer=tf.constant_initializer(0.0))
+
+        
+
+
 
 
 
