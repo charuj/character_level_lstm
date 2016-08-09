aa00f8596f45ca401d54d2dd36380a48e3ec8f62
charujaiswal <clmjaiswal@gmail.com>
2016-08-09 17:17:02 -0400

    Multi-layer/stacked LSTM

--- lstm.py
+++ lstm.py
@@ -27,13 +27,7 @@ from tensorflow.python.ops import rnn_cell
 
 # Parameters
 
-learning_rate = 0.001
 training_iters=100000
-batch_size= 50
-seq_length = 20 # number of steps to unroll the LSTM
-num_nodes = 512
-num_layers= 2
-
 
 # TF Graph input
 input_data= tf.placeholder(tf.float32, [batch_size, seq_length])
@@ -44,13 +38,18 @@ class Model():
         self.args= args
         args.batch_size= 50
         args.lstm_size= 512 # this is the number of nodes per hidden layer
-        args.seq_length= 20
+        args.seq_length= 20 # number of steps to unroll the LSTM
         args.num_layers= 2
+        args.learning_rate= 0.001
 
-
+        # Create multi-layer LSTM cell
         lstm= rnn_cell.BasicLSTMCell(args.lstm_size) # size of a single LSTM cell
         self.lstm= rnn_cell.MultiRNNCell([lstm]*args.num_layers)  # creating a multi-layer LSTM; the advantage of this is that the number of layers can be changed easily
+        self.initial_state= lstm.zero_state(args.batch_size,tf.float32)
 
+        # Create placeholders for inputs and targets
+        self.input_data= tf.placeholder(tf.float32,[args.batch_size, args.seq_length]) #TODO: should data be float or int?
+        self.targets = tf.placeholder(tf.float32,[args.batch_size,args.seq_length])
 
 
 
