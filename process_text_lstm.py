2bc5bc28fe3dacac4675e8577bf1c45f2481cc40
charujaiswal <clmjaiswal@gmail.com>
2016-08-11 14:37:44 -0400

    Open file in read mode; remove newline to prevent string from being turned into a list

--- process_text_lstm.py
+++ process_text_lstm.py
@@ -6,6 +6,19 @@ Code for processing text files for LSTM.
 - converts character strings into integer IDs
 - makes mini-batches with the inputs
 
+Inspired by how Udacity's Tensorflow LSTM tutorial converts characters to IDs and vice versa.
+See: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/udacity/6_lstm.ipynb
 
 '''
+
+import numpy as np
+
+
+# Load text file/corpus; I am loading the text file as a string variable, and removing newline. May need to play around with this
+with open('filename.txt', 'r') as myfile:
+    data= myfile.read().replace('\n', '') # remove new line. Prevents newlines from being turned into a list
+
+
+
+
 
