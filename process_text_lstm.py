fce0f0ab1b63dcd93859c446e17c9b74cdae46e4
charujaiswal <clmjaiswal@gmail.com>
2016-08-11 14:53:58 -0400

    Create validation and training set function

--- process_text_lstm.py
+++ process_text_lstm.py
@@ -14,9 +14,32 @@ See: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/ud
 import numpy as np
 
 
-# Load text file/corpus; I am loading the text file as a string variable, and removing newline. May need to play around with this
-with open('filename.txt', 'r') as myfile:
-    data= myfile.read().replace('\n', '') # remove new line. Prevents newlines from being turned into a list
+def read_date(filename):
+    '''
+    filename: textfile containing corpus.
+    Returns: data; text written to string.
+    '''
+
+    with open('filename.txt', 'r') as myfile:
+        data= myfile.read().replace('\n', '') # remove new line. Prevents newlines from being turned into a list
+
+    return data
+
+def create_sets(data, valid_size):
+    '''
+
+    :param data: corpus in string format
+    :param valid_size: desired size of validation set; a number of characters.
+    :return: validation set, training set, size of training set
+    '''
+
+    valid_set =  data[:valid_size]  # the first valid_size characters become the validation set
+    training_set= data[valid_size:]
+    train_size= len(training_set)
+
+    return valid_set, training_set, train_size
+
+
 
 
 
