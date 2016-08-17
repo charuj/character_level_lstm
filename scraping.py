c261c1da3e5d86df556ed523326621d544342b90
charujaiswal <clmjaiswal@gmail.com>
2016-08-17 14:32:39 -0400

    Read me for twitter scraping file

---
+++ scraping.py
@@ -0,0 +1,6 @@
+'''
+Code to scrape public tweets for a particular user, from Twitter's website.
+These tweets will then pass to the process_text_lstm.py file for processing into datasets,
+where they will eventually be used in the LSTM to generate more tweets in the style of the author.
+
+'''
