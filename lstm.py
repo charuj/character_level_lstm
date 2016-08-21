c1ee1452f95741c353c71c7747fb9f6176bc7332
charujaiswal <clmjaiswal@gmail.com>
2016-08-20 21:53:53 -0400

    Reshaping of variable tensors

--- lstm.py
+++ lstm.py
@@ -41,7 +41,7 @@ training_iters=100000
 
 class model():
     def __init__(self, config, is_training):
-        self.batch_size= batch_size= config.batch_size
+        self.batch_size= batch_size= config.num_batches
         self.num_steps= num_steps= config.num_steps
         hidden_size= config.hidden_size
         vocab_size= config.vocab_size
@@ -49,18 +49,18 @@ class model():
 
         # Create placeholders for inputs and targets
         self.input_data= tf.placeholder(tf.int32,[batch_size, num_steps])
-        self.targets = tf.placeholder(tf.int32,[batch_size,num_steps])
+        self.targets = tf.placeholder(tf.float32,[batch_size,num_steps])
 
         # Create multi-layer LSTM cell
         lstm_cell= tf.nn.rnn_cell.BasicLSTMCell(hidden_size) # using default forget_bias=1.0
-
-        # Add dropout
-        if is_training and config.keep_prob <1:
-            lstm_cell= tf.nn.rnn_cell.DropoutWrapper(lstm_cell,output_keep_prob=config.keep_prob) #dropout of the outputs based on keep_prob
+        #
+        # # Add dropout
+        # if is_training and config.keep_prob <1:
+        #     lstm_cell= tf.nn.rnn_cell.DropoutWrapper(lstm_cell,output_keep_prob=config.keep_prob) #dropout of the outputs based on keep_prob
 
         # Creating a multi-layer LSTM; the advantage of this is that the number of layers can be changed easily
         self.lstm= lstm= tf.nn.rnn_cell.MultiRNNCell([lstm_cell]*config.num_layers)
-        self.initial_state = lstm.zero_state(batch_size, tf.int32)
+        self.initial_state = lstm.zero_state(batch_size, tf.float32)
 
 
         with tf.variable_scope('lstm-'):
@@ -73,6 +73,8 @@ class model():
 
             # Create variable named 'weights'. Uses an initializer instead of passing the value directly as in tf.Variable
             weights= tf.get_variable('weights',[hidden_size, vocab_size], initializer=tf.random_normal_initializer())
+            weights= tf.expand_dims(weights,0)
+            weights= tf.tile(weights,[batch_size,1,1])
 
             # Create a variable named "biases". Initialize to value of 0
             biases = tf.get_variable('biases', vocab_size, initializer=tf.constant_initializer(0.0))
@@ -83,14 +85,20 @@ class model():
             inputs = tf.nn.embedding_lookup(embedding, self.input_data) #Character embeddings, where input_data =character IDs
 
             inputs = [tf.squeeze(inputs, [1]) for inputs in tf.split(1, num_steps, inputs)]  # build unrolled LSTM
-            if is_training and config.keep_prob <1:
-                inputs= tf.nn.dropout(inputs, config.keep_prob) # Apply dropout in inputs
+            # if is_training and config.keep_prob <1:
+            #
+            #     for i in range (len(inputs)):
+            #         inputs[i]= tf.nn.dropout(inputs[i], config.keep_prob) # Apply dropout in inputs
+            #
 
         outputs, state = rnn.rnn(lstm, inputs, initial_state=self.initial_state)
-        # TODO: May need to reshape the output
+        outputs= tf.reshape(tf.concat(1,outputs), [-1, config.num_unrollings,config.hidden_size])
 
-        self.logits= tf.matmul(outputs, weights) + biases
+        # TODO: May need to reshape the output
+        self.logits= tf.batch_matmul(outputs,weights) + biases
+       # self.logits= tf.matmul(outputs, weights) + biases
         #TODO: do I need self.probs = tf.nn.softmax(self.logits)  ???
+
 
         #Define loss and optimizer
         self.loss= tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.logits, self.targets))
@@ -134,19 +142,19 @@ def main(config, model, trainx, trainy, validx, validy):
     init= tf.initialize_all_variables()
     data_len= len(trainx)
     batch_len = data_len // config.num_batches
-    num_epochs=(batch_len -1)//config.num_unrollings
-
+    num_epochs=(batch_len -1)//config.num_unrollings  #TODO: Hardcode number of epochs
+    num_epochs = 2
     # Launch the graph
     with tf.Session() as sess:
         sess.run(init)
         step=1
         for i in range (num_epochs):
-            state= model.initial_state.eval()
+            state= model.initial_state.eval() #TODO: need to "instantiate" classes as objects before you use their functions like this, unless they are "static" functions
             for j in range(config.num_batches):
                 # the data is already shaped to account for batches.
                 # data is in the shape [num_batches, num_unrollings]
                 # therefore, loop over the rows of the input and target matrices
-                x = trainx[j, :]
+                x = trainx[j, :] #TODO: Check if x is actually one "batch" of data (same for y)
                 y= trainy[j,:]
                 feed = {model.input_data: x, model.targets:y, model.initial_state:state}
                 train_loss, state, _, accuracy = sess.run([model.loss, model.final_state, model.optimizer, model.accuracy],feed_dict=feed )
@@ -162,6 +170,8 @@ def main(config, model, trainx, trainy, validx, validy):
 
 
 ## MAIN ###
+configuration= config()
+lstmmodel = model(configuration, is_training= True)
 main(config, model, trainx, trainy, validx, validy)
 
 
